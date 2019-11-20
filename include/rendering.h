cbe16af401733bc730d039acc45e8842bcea017d
Gabriel Simonetti <gabrielsimonetti11@gmail.com>
2019-11-20 01:01:46 -0300

    Code cleaning

---
+++ include/rendering.h
@@ -0,0 +1,14 @@
+#ifndef _RENDERING_H
+#define _RENDERING_H
+
+// Definimos uma estrutura que armazenará dados necessários para renderizar
+// cada objeto da cena virtual.
+struct SceneObject
+{
+    const char*  name;        // Nome do objeto
+    void*        first_index; // Índice do primeiro vértice dentro do vetor indices[] definido em BuildTriangles()
+    int          num_indices; // Número de índices do objeto dentro do vetor indices[] definido em BuildTriangles()
+    GLenum       rendering_mode; // Modo de rasterização (GL_TRIANGLES, GL_TRIANGLE_STRIP, etc.)
+};
+
+#endif // _RENDERING_H
--- src/main.cpp
+++ src/main.cpp
@@ -8,13 +8,6 @@
 //                   TRABALHO FINAL
 //
 
-// Arquivos "headers" padrões de C podem ser incluídos em um
-// programa C++, sendo necessário somente adicionar o caractere
-// "c" antes de seu nome, e remover o sufixo ".h". Exemplo:
-//    #include <stdio.h> // Em C
-//  vira
-//    #include <cstdio> // Em C++
-//
 #include <cmath>
 #include <cstdio>
 #include <cstdlib>
@@ -38,6 +31,7 @@
 // Headers locais, definidos na pasta "include/"
 #include "utils.h"
 #include "matrices.h"
+#include "rendering.h"
 
 // Headers da biblioteca para carregar modelos obj
 #include <tiny_obj_loader.h>
@@ -71,7 +65,6 @@ struct ObjModel
     }
 };
 
-
 // Declaração de funções utilizadas para pilha de matrizes de modelagem.
 void PushMatrix(glm::mat4 M);
 void PopMatrix(glm::mat4& M);
@@ -98,9 +91,6 @@ void TextRendering_PrintMatrixVectorProductDivW(GLFWwindow* window, glm::mat4 M,
 
 // Funções abaixo renderizam como texto na janela OpenGL algumas matrizes e
 // outras informações do programa. Definidas após main().
-void TextRendering_ShowModelViewProjection(GLFWwindow* window, glm::mat4 projection, glm::mat4 view, glm::mat4 model, glm::vec4 p_model);
-void TextRendering_ShowEulerAngles(GLFWwindow* window);
-void TextRendering_ShowProjection(GLFWwindow* window);
 void TextRendering_ShowFramesPerSecond(GLFWwindow* window);
 
 // Funções callback para comunicação com o sistema operacional e interação do
@@ -112,16 +102,6 @@ void MouseButtonCallback(GLFWwindow* window, int button, int action, int mods);
 void CursorPosCallback(GLFWwindow* window, double xpos, double ypos);
 void ScrollCallback(GLFWwindow* window, double xoffset, double yoffset);
 
-// Definimos uma estrutura que armazenará dados necessários para renderizar
-// cada objeto da cena virtual.
-struct SceneObject
-{
-    const char*  name;        // Nome do objeto
-    void*        first_index; // Índice do primeiro vértice dentro do vetor indices[] definido em BuildTriangles()
-    int          num_indices; // Número de índices do objeto dentro do vetor indices[] definido em BuildTriangles()
-    GLenum       rendering_mode; // Modo de rasterização (GL_TRIANGLES, GL_TRIANGLE_STRIP, etc.)
-};
-
 // Abaixo definimos variáveis globais utilizadas em várias funções do código.
 
 // A cena virtual é uma lista de objetos nomeados, guardados em um dicionário
@@ -132,11 +112,6 @@ std::map<const char*, SceneObject> g_VirtualScene;
 
 // Razão de proporção da janela (largura/altura). Veja função FramebufferSizeCallback().
 float g_ScreenRatio = 1.0f;
-
-// Ângulos de Euler que controlam a rotação de um dos cubos da cena virtual
-float g_AngleX = 0.0f;
-float g_AngleY = 0.0f;
-float g_AngleZ = 0.0f;
 
 // "g_LeftMouseButtonPressed = true" se o usuário está com o botão esquerdo do mouse
 // pressionado no momento atual. Veja função MouseButtonCallback().
@@ -190,7 +165,7 @@ public:
         render_as_black_uniform = glGetUniformLocation(program_id, "render_as_black");
     };
 
-    void placeOnScene(float positionX, float positionZ)
+    void placeOnScene(std::map<const char*, SceneObject> g_VirtualScene, float positionX, float positionZ)
     {
         glm::mat4 model;
         model = Matrix_Translate(positionX, 0.0f, positionZ);
@@ -431,14 +406,14 @@ int main()
         glUniformMatrix4fv(projection_uniform , 1 , GL_FALSE , glm::value_ptr(projection));
 
         // Vamos desenhar as instâncias (cópias) do cubo
-        int mapSize = 3;
+        int mapSize = 6;
         float transZ = 0.0f;
         float transX = 0.0f;
 
         for (int i = 1; i <= mapSize * 2 + 3; ++i)
         {
             Block new_block(true, program_id);
-            new_block.placeOnScene(-transX, -transZ);
+            new_block.placeOnScene(g_VirtualScene, -transX, -transZ);
             transX += DIST_BETWEEN_CUBES / 2;
         }
 
@@ -448,7 +423,7 @@ int main()
         for (int i = 1; i <= mapSize * 2 + 3; ++i)
         {
             Block new_block(true, program_id);
-            new_block.placeOnScene(-transX, -transZ);
+            new_block.placeOnScene(g_VirtualScene, -transX, -transZ);
             transX += DIST_BETWEEN_CUBES / 2;
         }
 
@@ -458,7 +433,7 @@ int main()
         for (int i = 1; i <= mapSize * 2 + 1; ++i)
         {
             Block new_block(true, program_id);
-            new_block.placeOnScene(-transX, -transZ);
+            new_block.placeOnScene(g_VirtualScene, -transX, -transZ);
             transZ += DIST_BETWEEN_CUBES / 2;
         }
 
@@ -468,7 +443,7 @@ int main()
         for (int i = 1; i <= mapSize * 2 + 1; ++i)
         {
             Block new_block(true, program_id);
-            new_block.placeOnScene(-transX, -transZ);
+            new_block.placeOnScene(g_VirtualScene, -transX, -transZ);
             transZ += DIST_BETWEEN_CUBES / 2;
         }
 
@@ -482,25 +457,28 @@ int main()
             for (int i = 1; i <= mapSize; ++i)
             {
                 Block new_block(true, program_id);
-                new_block.placeOnScene(-transX, -transZ);
+                new_block.placeOnScene(g_VirtualScene, -transX, -transZ);
                 transX += DIST_BETWEEN_CUBES;
             }
         }
 
-        transZ = 0.0f;
-        transX = DIST_BETWEEN_CUBES / 2;
-
-        for (int i = 1; i <= mapSize; ++i)
-        {
-            transZ += DIST_BETWEEN_CUBES;
-            transX = DIST_BETWEEN_CUBES / 2;
-            for (int j = 1; j <= mapSize + 1; ++j)
-            {
-                Block new_block(false, program_id);
-                new_block.placeOnScene(-transX, -transZ);
-                transX += DIST_BETWEEN_CUBES;
-            }
-        }
+//        transZ = 0.0f;
+//        transX = DIST_BETWEEN_CUBES / 2;
+//
+//        for (int i = 0; i < mapSize * 2 + 1; ++i)
+//        {
+//            transX = DIST_BETWEEN_CUBES / 2;
+//            transZ += DIST_BETWEEN_CUBES / 2;
+//            for (int j = 0; j < mapSize * 2 + 1; ++j)
+//            {
+//                if ((i + j) % 2 != 0)
+//                {
+//                    Block new_block(false, program_id);
+//                    new_block.placeOnScene(g_VirtualScene, -transX, -transZ);
+//                    transX += DIST_BETWEEN_CUBES;
+//                }
+//            }
+//        }
 
         // Agora queremos desenhar os eixos XYZ de coordenadas GLOBAIS.
         // Para tanto, colocamos a matriz de modelagem igual à identidade.
@@ -1056,56 +1034,6 @@ void KeyCallback(GLFWwindow* window, int key, int scancode, int action, int mod)
     if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
         glfwSetWindowShouldClose(window, GL_TRUE);
 
-    // O código abaixo implementa a seguinte lógica:
-    //   Se apertar tecla X       então g_AngleX += delta;
-    //   Se apertar tecla shift+X então g_AngleX -= delta;
-    //   Se apertar tecla Y       então g_AngleY += delta;
-    //   Se apertar tecla shift+Y então g_AngleY -= delta;
-    //   Se apertar tecla Z       então g_AngleZ += delta;
-    //   Se apertar tecla shift+Z então g_AngleZ -= delta;
-
-    float delta = 3.141592 / 16; // 22.5 graus, em radianos.
-
-    if (key == GLFW_KEY_X && action == GLFW_PRESS)
-    {
-        g_AngleX += (mod & GLFW_MOD_SHIFT) ? -delta : delta;
-    }
-
-    if (key == GLFW_KEY_Y && action == GLFW_PRESS)
-    {
-        g_AngleY += (mod & GLFW_MOD_SHIFT) ? -delta : delta;
-    }
-    if (key == GLFW_KEY_Z && action == GLFW_PRESS)
-    {
-        g_AngleZ += (mod & GLFW_MOD_SHIFT) ? -delta : delta;
-    }
-
-    // Se o usuário apertar a tecla espaço, resetamos os ângulos de Euler para zero.
-    if (key == GLFW_KEY_SPACE && action == GLFW_PRESS)
-    {
-        g_AngleX = 0.0f;
-        g_AngleY = 0.0f;
-        g_AngleZ = 0.0f;
-    }
-
-    // Se o usuário apertar a tecla P, utilizamos projeção perspectiva.
-    //if (key == GLFW_KEY_P && action == GLFW_PRESS)
-    //{
-    //    g_UsePerspectiveProjection = true;
-    //}
-
-    // Se o usuário apertar a tecla O, utilizamos projeção ortográfica.
-    //if (key == GLFW_KEY_O && action == GLFW_PRESS)
-    //{
-    //    g_UsePerspectiveProjection = false;
-    //}
-
-    // Se o usuário apertar a tecla H, fazemos um "toggle" do texto informativo mostrado na tela.
-    //if (key == GLFW_KEY_H && action == GLFW_PRESS)
-    //{
-    //    g_ShowInfoText = !g_ShowInfoText;
-    //}
-
     // Se o usuário apertar a tecla W, a câmera deve se movimentar para frente.
     if (key == GLFW_KEY_W && action == GLFW_PRESS)
     {
@@ -1151,98 +1079,6 @@ void KeyCallback(GLFWwindow* window, int key, int scancode, int action, int mod)
 void ErrorCallback(int error, const char* description)
 {
     fprintf(stderr, "ERROR: GLFW: %s\n", description);
-}
-
-// Esta função recebe um vértice com coordenadas de modelo p_model e passa o
-// mesmo por todos os sistemas de coordenadas armazenados nas matrizes model,
-// view, e projection; e escreve na tela as matrizes e pontos resultantes
-// dessas transformações.
-void TextRendering_ShowModelViewProjection(
-    GLFWwindow* window,
-    glm::mat4 projection,
-    glm::mat4 view,
-    glm::mat4 model,
-    glm::vec4 p_model
-)
-{
-    if ( !g_ShowInfoText )
-        return;
-
-    glm::vec4 p_world = model*p_model;
-    glm::vec4 p_camera = view*p_world;
-    glm::vec4 p_clip = projection*p_camera;
-    glm::vec4 p_ndc = p_clip / p_clip.w;
-
-    float pad = TextRendering_LineHeight(window);
-
-    TextRendering_PrintString(window, " Model matrix             Model     In World Coords.", -1.0f, 1.0f-pad, 1.0f);
-    TextRendering_PrintMatrixVectorProduct(window, model, p_model, -1.0f, 1.0f-2*pad, 1.0f);
-
-    TextRendering_PrintString(window, "                                        |  ", -1.0f, 1.0f-6*pad, 1.0f);
-    TextRendering_PrintString(window, "                            .-----------'  ", -1.0f, 1.0f-7*pad, 1.0f);
-    TextRendering_PrintString(window, "                            V              ", -1.0f, 1.0f-8*pad, 1.0f);
-
-    TextRendering_PrintString(window, " View matrix              World     In Camera Coords.", -1.0f, 1.0f-9*pad, 1.0f);
-    TextRendering_PrintMatrixVectorProduct(window, view, p_world, -1.0f, 1.0f-10*pad, 1.0f);
-
-    TextRendering_PrintString(window, "                                        |  ", -1.0f, 1.0f-14*pad, 1.0f);
-    TextRendering_PrintString(window, "                            .-----------'  ", -1.0f, 1.0f-15*pad, 1.0f);
-    TextRendering_PrintString(window, "                            V              ", -1.0f, 1.0f-16*pad, 1.0f);
-
-    TextRendering_PrintString(window, " Projection matrix        Camera                    In NDC", -1.0f, 1.0f-17*pad, 1.0f);
-    TextRendering_PrintMatrixVectorProductDivW(window, projection, p_camera, -1.0f, 1.0f-18*pad, 1.0f);
-
-    int width, height;
-    glfwGetFramebufferSize(window, &width, &height);
-
-    glm::vec2 a = glm::vec2(-1, -1);
-    glm::vec2 b = glm::vec2(+1, +1);
-    glm::vec2 p = glm::vec2( 0,  0);
-    glm::vec2 q = glm::vec2(width, height);
-
-    glm::mat4 viewport_mapping = Matrix(
-        (q.x - p.x)/(b.x-a.x), 0.0f, 0.0f, (b.x*p.x - a.x*q.x)/(b.x-a.x),
-        0.0f, (q.y - p.y)/(b.y-a.y), 0.0f, (b.y*p.y - a.y*q.y)/(b.y-a.y),
-        0.0f , 0.0f , 1.0f , 0.0f ,
-        0.0f , 0.0f , 0.0f , 1.0f
-    );
-
-    TextRendering_PrintString(window, "                                                       |  ", -1.0f, 1.0f-22*pad, 1.0f);
-    TextRendering_PrintString(window, "                            .--------------------------'  ", -1.0f, 1.0f-23*pad, 1.0f);
-    TextRendering_PrintString(window, "                            V                           ", -1.0f, 1.0f-24*pad, 1.0f);
-
-    TextRendering_PrintString(window, " Viewport matrix           NDC      In Pixel Coords.", -1.0f, 1.0f-25*pad, 1.0f);
-    TextRendering_PrintMatrixVectorProductMoreDigits(window, viewport_mapping, p_ndc, -1.0f, 1.0f-26*pad, 1.0f);
-}
-
-// Escrevemos na tela os ângulos de Euler definidos nas variáveis globais
-// g_AngleX, g_AngleY, e g_AngleZ.
-void TextRendering_ShowEulerAngles(GLFWwindow* window)
-{
-    if ( !g_ShowInfoText )
-        return;
-
-    float pad = TextRendering_LineHeight(window);
-
-    char buffer[80];
-    snprintf(buffer, 80, "Euler Angles rotation matrix = Z(%.2f)*Y(%.2f)*X(%.2f)\n", g_AngleZ, g_AngleY, g_AngleX);
-
-    TextRendering_PrintString(window, buffer, -1.0f+pad/10, -1.0f+2*pad/10, 1.0f);
-}
-
-// Escrevemos na tela qual matriz de projeção está sendo utilizada.
-void TextRendering_ShowProjection(GLFWwindow* window)
-{
-    if ( !g_ShowInfoText )
-        return;
-
-    float lineheight = TextRendering_LineHeight(window);
-    float charwidth = TextRendering_CharWidth(window);
-
-    if ( g_UsePerspectiveProjection )
-        TextRendering_PrintString(window, "Perspective", 1.0f-13*charwidth, -1.0f+2*lineheight/10, 1.0f);
-    else
-        TextRendering_PrintString(window, "Orthographic", 1.0f-13*charwidth, -1.0f+2*lineheight/10, 1.0f);
 }
 
 // Escrevemos na tela o número de quadros renderizados por segundo (frames per
